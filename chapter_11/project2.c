11176035322733c9ce79253a0a04eaa94334c16b
Formasit chijoh <formasitf@gmail.com>
2021-08-15 08:05:19 -0400

    Flight reservation program.

--- chapter_11/project2.c
+++ chapter_11/project2.c
@@ -1,24 +1,28 @@
 #include <stdio.h>
 void find_closest_flight(int desired_time,int* departure_time,int*arrival_time);
  int main(){
-     int hr,min,departure_time, arrival_time,desired_time;
+     int hr,min,departure_time,
+      arrival_time,desired_time;
+
        printf("Enter the time in the form hr:min\n");
          scanf("%d:%d",&hr,&min);
         find_closest_flight(desired_time,&departure_time,&arrival_time);
         return 0;
 
  }
- void flight_closest_flight(int desired_time,int*departure_time,int*arrival_time)
- {int hr,min;
+ void find_closest_flight(int desired_time,int*departure_time,int*arrival_time)
+ {
+     int hr,min;
      desired_time = (hr*60) + min;
-      if(desired_time >= 480 && desired_time <= 520){
+      
+      if(desired_time >= 480 && desired_time < 520){
           printf("closest departure time is 8:00am,arriving at 10:16am\n");
       }
-      else if(desired_time >= 502 && desired_time <= 620 )
+      else if(desired_time >= 520 && desired_time < 620 )
       {
           printf("closest departure time is 9:43pm,arrival at 11:52\n");
       }
-      else if(desired_time >= 620 && desired_time <= 700){
+      else if(desired_time > 620 && desired_time < 700){
           printf("closest departure time is 11:19am.arrival time is 1:32pm\n");
 
       }
