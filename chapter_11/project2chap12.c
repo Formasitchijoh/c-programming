9cf121c41dd41062aae3f6377899238b696a7596
Formasit chijoh <formasitf@gmail.com>
2021-08-26 21:59:10 -0400

    This program check if a word is palindrome

--- chapter_11/project2chap12.c
+++ chapter_11/project2chap12.c
@@ -12,22 +12,25 @@ void read_drome(char array[]);
   }
    void give_input(){
        printf("Enter the input to be searched\n");
-   }
+   }// This function indexes the different position in the string and
+   // checks if they are equal
   void read_drome(char array[]){
   int c,j = 0;
   int i = N- 1;
-   while((c=getchar()) != '\n'  &&  j< N){
+   while((c=getchar()) != '\n'  &&  j< N)// Reading the character one after the other from stdin and storing 
+   //it in an array
+   {
    
        array[i] = c;
        j++;
  }
     while(i>j){
-       if(array[i--] != array[j++])
+       if(array[i--] != array[j++]) //indexing the diffferent position of th array
        {
            printf(" %s palindrome \n",array);
                 }
    
-      else /*if(array[i] != array[j])*/
+      else 
       {
           printf(" %s Not palindrom\n",array);
       }
